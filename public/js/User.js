45fc0436f0a6e1e5a4e3f5a2e41b8bb459eb2147
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-10-29 18:00:56 +0400

    added new set

--- public/js/User.js
+++ public/js/User.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
     $.get('/api/User/MainData').then(response => {
         $('#name_set').text(response.data.firstName + ' ' + response.data.lastName);
         $('#country_set').text(response.data.country);
+        $('#phone_set').text(response.data.phone);
     })
 })
