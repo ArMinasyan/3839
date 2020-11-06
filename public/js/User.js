d29fcac090b84ce572c8bc5c17b2a35358f44697
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2020-11-06 12:17:43 +0400

    added service push_div

--- public/js/User.js
+++ public/js/User.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).on('change', '#imageUpload2', e => {
     $(this).val('');
     const form = new FormData();
@@ -16,7 +13,8 @@ $(document).on('change', '#imageUpload2', e => {
             if (response.err) swal({
                 text: response.err,
                 closeOnClickOutside: false
-            }); else {
+            });
+            else {
                 $('#profileimg_set').attr('src', 'ProfileImages/' + response.profile_img);
                 $('.logoAvatar').css('border', 'none');
             }
@@ -45,7 +43,7 @@ const allSocialIcons = document.getElementsByClassName('socialIcon');
 
 let changed = false;
 let old_desc = "";
-
+let new_child_index = 0;
 $(document).on('click', '#edit_description', e => {
     old_desc = $('#description_set').text();
     $('#description_set').attr('contenteditable', true);
@@ -63,8 +61,12 @@ $(document).on('click', '#edit_description', e => {
 
 $(document).on('click', '#save_descriptionChange', e => {
     const descritpion = $('#description_set').text();
-    $.post('/api/User/UpdateDescription', { desc: descritpion }).then(response => {
-        if (response.updated) Swal.fire({ title: 'Information Successfully saved' }).then(() => {
+    $.post('/api/User/UpdateDescription', {
+        desc: descritpion
+    }).then(response => {
+        if (response.updated) Swal.fire({
+            title: 'Information Successfully saved'
+        }).then(() => {
             $('#description_set').removeAttr('contenteditable');
             $('#description_set').css({
                 'border': 'none',
@@ -119,10 +121,14 @@ $(document).on('click', '#edit_links', e => {
         confirmButtonText: 'Save',
     }).then(e => {
         if (e.isConfirmed) {
-            $.post('/api/User/UpdateSocialLinks', { updated_links }).then(response => {
+            $.post('/api/User/UpdateSocialLinks', {
+                updated_links
+            }).then(response => {
                 let first = '';
                 if (response.updated) {
-                    Swal.fire({ title: 'Information Successfully saved' }).then(e => {
+                    Swal.fire({
+                        title: 'Information Successfully saved'
+                    }).then(e => {
 
                         Object.keys(updated_links).forEach((key, index) => {
                             $('#' + key + '_link_set').text(updated_links[key]);
@@ -139,6 +145,7 @@ $(document).on('click', '#edit_links', e => {
     })
 })
 $(document).on('click', '#edit_main', e => {
+
     Swal.fire({
         customClass: {
             container: 'main_info_swal'
@@ -171,6 +178,106 @@ $(document).on('click', '#edit_main', e => {
         <div class="swal-container--sub_1">Phone Number</div>
         <div class="swal-container--sub_2"><input type="text" value='${$('#phone_set').text()}' name='Si_phone'/></div>
         </div>
+        
+        
+        <ul id="services_list" hidden>
+        <li>
+            <div>Hair removal</div>
+            <ul>
+                <li>Waxing</li>
+                <li>Sugaring</li>
+                <li>Threading</li>
+                <li>Laser</li>
+            </ul>
+        </li>
+        <li>
+            <div> Nails</div>
+            <ul>
+                <li>feet</li>
+                <li>hands</li>
+                <li>
+                    <div>Medical pedicure</div>
+                    <ul>
+                        <li>chiropody </li>
+                        <li>podiatry</li>
+                    </ul>
+                </li>
+                <li> Manicure</li>
+                <li> Pedicure</li>
+                <li> Nail art</li>
+                <li> Shellac</li>
+                <li> Acrylics</li>
+                <li> Natural products</li>
+                <li> Wax treatments</li>
+                <li> Nail conditioning treatments</li>
+            </ul>
+        </li>
+        <li>Make up</li>
+        <li>
+            <div>Hair</div>
+            <ul>
+                <li>Cut</li>
+                <li>
+                    <div>Colour</div>
+                    <ul>
+                        <li>lowlights</li>
+                        <li>Tints</li>
+                        <li>high</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li> Perming</li>
+        <li> Chemical hair straightening</li>
+        <li> Wash and blow dry</li>
+        <li> Hair up/ dressing hair for events</li>
+        <li> Barbering</li>
+        <li>
+            <div>Osteopathy</div>
+            <ul>
+                <li>Spinal/ joint/ cranial manipulation</li>
+                <li> Consultation</li>
+                <li> Face to Face booking</li>
+                <li> Video booking</li>
+            </ul>
+        </li>
+        <li> Councelling</li>
+        <li> Anxiety</li>
+        <li> Trauma</li>
+        <li> Relationships</li>
+        <li> Depression</li>
+        <li> Low moods</li>
+        <li> CBT</li>
+        <li> Psychotherapy</li>
+        <li> Face to Face booking</li>
+        <li> Online booking</li>
+        <li>
+            <div>Mindfulness</div>
+            <ul>
+                <li> Happy thinking</li>
+                <li> Relaxing</li>
+                <li> Invigorating</li>
+                <li> Destressing</li>
+                <li> Face to face</li>
+                <li> Online</li>
+            </ul>
+        </li>
+        <li> Life coaching</li>
+        <li> Help client to find self inspiration</li>
+        <li> Face to face</li>
+        <li> Online booking</li>
+        <li> Business coaching</li>
+        <li> Help with mindset</li>
+        <li> Creative thinking</li>
+        <li> Strategy</li>
+        <li> Marketing</li>
+        <li> Sales</li>
+        <li> Networking</li>
+        <li> Face to Face</li>
+        <li> Online</li>
+    </ul>
+    
+    </div>
         </div> `,
         showCancelButton: true,
         //  allowOutsideClick: false,
@@ -178,9 +285,13 @@ $(document).on('click', '#edit_main', e => {
 
     }).then(e => {
         if (e.isConfirmed)
-            $.post('/api/User/UpdateMainInfo', { updated_data }).then(response => {
+            $.post('/api/User/UpdateMainInfo', {
+                updated_data
+            }).then(response => {
                 if (response.updated) {
-                    Swal.fire({ title: 'Information Successfully saved' }).then(e => {
+                    Swal.fire({
+                        title: 'Information Successfully saved'
+                    }).then(e => {
                         Object.keys(updated_data).forEach(key => {
                             if (key !== 'firstName' || key !== 'lastName') $('#' + key + '_set').text(updated_data[key]);
 
@@ -189,8 +300,39 @@ $(document).on('click', '#edit_main', e => {
                     });
                 }
             })
-    })
+    });
+    $('#services_list').menu({
+        position: {
+            my: "left top",
+            at: "right+5 top-3"
+        },
+
+    });
 });
+
+const addService = (id) => {
+    let new_child = document.createElement('div');
+    new_child.setAttribute('contenteditable', true);
+    new_child.setAttribute('class', 'services_list_push_child');
+    new_child_index++;
+    $(id).append(new_child)
+}
+
+$(document).on('click', '#service_list_push', e => {
+    if ($('#service_list_push').children().length == 0) {
+        addService('#service_list_push');
+        $('.services_list_push_child').focus();
+    } else
+    if (e.target.id == 'service_list_push') addService('#service_list_push');
+})
+
+$(document).on('keypress', '#service_list_push', e => {
+    if (e.which == 13) {
+        addService('#service_list_push');
+        $('.services_list_push_child').next().focus();
+        return false;
+    }
+})
 
 $(document).on('change', 'input[name=twitter],input[name=linkedin],input[name=instagram],input[name=facebook]', e => {
     updated_links[e.target.name] = e.target.value.trim();
@@ -216,7 +358,8 @@ $(document).on('change', '#imageUpload', e => {
             if (response.err) swal({
                 text: response.err,
                 closeOnClickOutside: false
-            }); else {
+            });
+            else {
                 $('#logoimg_set').attr('src', 'LogoImages/' + response.logo_img);
                 $('.logoAvatar').css('border', 'none');
             }
@@ -235,10 +378,7 @@ $(document).ready(function () {
         }
     });
 
-    $('#services_list').menu({
-        position: { my: "left top", at: "right+5 top-3" },
 
-    });
 
     $('#services_list').on('click', function (e) {
         console.log(e.target.innerText);
